bb98c61f38bbca6946c0bc75ffc9df3390bd14d7
FutureTroy <troymbrown4@gmail.com>
2016-11-07 18:43:57 -0500

    config v2

--- website/php/config.php
+++ website/php/config.php
@@ -1,7 +1,7 @@
 <?php
 	define('dbserver','24.99.201.43');
-	define('dbusername','techsavvyadmin');
-	define('dbpassword','magnificentseven2016');
+	define('dbusername','pma');
+	define('dbpassword','testingtesting123');
 	define('dbdatabase','techsavvyadmin');
 	
 	//Creates a connection the the database that can be used in any other php file
