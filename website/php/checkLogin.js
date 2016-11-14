0ba90a43866026b1f19c4e64b8966fe76ef6e553
Adam Murr <adamcmurr@gmail.com>
2016-11-13 19:36:35 -0500

    Changed reference to checkLogin.php

---
+++ website/php/checkLogin.js
@@ -0,0 +1,29 @@
+function check(){
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+	}
+	else {
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			if(xmlhttp.responseText != "false")
+			{
+				changeHTML(xmlhttp.responseText);
+			}
+		}
+	}
+	xmlhttp.open("GET", "./php/checkLogin.php", true);
+	xmlhttp.send();
+}
+
+function changeHTML(user){
+	var btnSignIn = document.getElementById("btnSignIn");
+	var username = document.getElementById("loggedInUser");
+	
+	btnSignIn.style.display = "none";
+	username.innerHTML = user;
+}
+
+window.onload = check();
